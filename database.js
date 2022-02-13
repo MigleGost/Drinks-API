3efe8a3546794fdd61aec0014853e93135396a12
MigleGost <89936436+MigleGost@users.noreply.github.com>
2022-02-13 12:41:17 +0200

    Update database.js
    -All functions for database filtering completed

--- database.js
+++ database.js
@@ -7,38 +7,74 @@ let connection = mysql.createConnection({
     database: 'sql11471058'
 })
 
-connection.connect(function(err) {
-    if(err) {
+connection.connect(function (err) {
+    if (err) {
         return console.error('error: ' + err.message)
     }
     console.log('Connected to the MySQL server.')
 })
 
 function getByName(drinkName) {
-connection.query("SELECT * FROM `TABLE 1` WHERE `strDrink` LIKE '"+drinkName+"'", function(err, result)
-{
-    if(err) {
-        return console.error('error: ' + err.message)
-    }
-    console.log(result)
-})}
-
-function getByFilter(category, ingredient) {
-    connection.query("SELECT * FROM `TABLE 1` WHERE `strCategory` LIKE '"+drinkName+"'", function(err, result)
-    {
-        if(err) {
+    connection.query("SELECT * FROM `TABLE 1` WHERE `strDrink` LIKE '" + drinkName + "'", function (err, result) {
+        if (err) {
             return console.error('error: ' + err.message)
         }
         console.log(result)
-    })}
+    })
+}
 
-function filterAlcoholic(alcoholic) {
-connection.query("SELECT * FROM `TABLE 1` WHERE `strAlcoholic` LIKE '"+alcoholic+"'", function(err, result)
-{
-    if(err) {
-        return console.error('error: ' + err.message)
-    }
-    console.log(result)
-})}
+function getByFilter(alcoholic, category, glass, ingredient) {
+    connection.query("SELECT * FROM `TABLE 1` WHERE `strAlcoholic` LIKE '" + alcoholic + "' AND `strCategory` LIKE '" + category + "' AND `strGlass` LIKE '" + glass + "' AND `strIngredient1` LIKE '" + ingredient + "' ", function (err, result) {
+        if (err) {
+            return console.error('error: ' + err.message)
+        }
+        console.log(result)
+    })
+}
+
+function getByAlcoholic(alcoholic) {
+    connection.query("SELECT * FROM `TABLE 1` WHERE `strAlcoholic` LIKE '" + alcoholic + "'", function (err, result) {
+        if (err) {
+            return console.error('error: ' + err.message)
+        }
+        console.log(result)
+    })
+}
+
+function getByCategory(category) {
+    connection.query("SELECT * FROM `TABLE 1` WHERE `strCaregory` LIKE '" + category + "'", function (err, result) {
+        if (err) {
+            return console.error('error: ' + err.message)
+        }
+        console.log(result)
+    })
+}
+
+function getOneRandom() {
+    connection.query("SELECT * FROM `TABLE 1` ORDER BY RAND() LIMIT 1", function (err, result) {
+        if (err) {
+            return console.error('error: ' + err.message)
+        }
+        console.log(result)
+    })
+}
+
+function getXRandom(x) {
+    connection.query("SELECT * FROM `TABLE 1` ORDER BY RAND() LIMIT "+x+"", function (err, result) {
+        if (err) {
+            return console.error('error: ' + err.message)
+        }
+        console.log(result)
+    })
+}
+
+function getCategoryRandom(category) {
+    connection.query("SELECT * FROM `TABLE 1` WHERE `strCategory` LIKE '"+category+"' ORDER BY RAND() LIMIT 1", function (err, result) {
+        if (err) {
+            return console.error('error: ' + err.message)
+        }
+        console.log(result)
+    })
+}
 
 connection.end()
